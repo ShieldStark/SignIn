6cdc637ae46b55341f5e6508a0100089135a6803
Piyush <piyushabhiranjan30@gmail.com>
2022-08-15 18:20:57 +0530

    Backend

--- app/src/main/java/com/example/signin/MainActivity.java
+++ app/src/main/java/com/example/signin/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.signin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView userName;
+    private EditText passWord;
+    private Button loginButton;
+    private TextView forgot;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userName=findViewById(R.id.username);
+        passWord=findViewById(R.id.password);
+        loginButton=findViewById(R.id.button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(userName.getText().toString().equalsIgnoreCase("admin")&&passWord.getText().toString().equals("password")){
+                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
